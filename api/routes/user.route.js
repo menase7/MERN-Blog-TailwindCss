f83f7f38982404d19093198fbec05c214df22120
Menase7 <menasedebel7@gmail.com>
2024-04-04 11:08:58 +0300

    delete user api route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,9 +1,11 @@
 import expres from 'express'
 import { verifyToken } from '../utils/verifyUser';
+import { deleteUser, updateUser } from '../controllers/user.controller';
 
 const router = expres.Router();
 
 router.put('/update/:userId',verifyToken, updateUser);
+router.delete('/delete/:userId',verifyToken, deleteUser);
 
 export default router;
 
