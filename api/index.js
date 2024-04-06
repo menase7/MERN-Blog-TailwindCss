6c3287903d639d71cdbafe5af204cd0ab4f77d7d
Menase7 <menasedebel7@gmail.com>
2024-04-06 04:29:29 +0300

    add create a post  api route

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import authRoute from './routes/auth.route.js';
 import userRoute from './routes/user.route.js';
+import postRoute from './routes/post.route.js'
 import cookieParser from 'cookie-parser';
 
 
@@ -31,6 +32,7 @@ app.listen(PORT, () => {
 
 app.use('/api/auth', authRoute);
 app.use('/api/user', userRoute);
+app.use('/api/post', postRoute);
 
 
 app.use((err, req, res, next)=>{
