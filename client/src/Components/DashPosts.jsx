c48360cc387968622ede838d43ac8e6514875b69
Menase7 <menasedebel7@gmail.com>
2024-04-08 11:13:06 +0300

    add show more functionality to the posts results of the dashboard

--- client/src/Components/DashPosts.jsx
+++ client/src/Components/DashPosts.jsx
@@ -6,6 +6,7 @@ import { Link } from "react-router-dom";
 export default function DashPosts() {
   const { currentUser } = useSelector((state) => state.user);
   const [userPosts, setUserPosts] = useState([]);
+  const [showMore, setShowMore] = useState(true);
   console.log(userPosts);
   useEffect(() => {
     const fetchPosts = async () => {
@@ -15,8 +16,10 @@ export default function DashPosts() {
         );
         const data = await res.json();
         if (res.ok) {
-          console.log(data.posts);
           setUserPosts(data.posts);
+          if(data.posts.length < 9){
+            setShowMore(false);
+          }
         }
       } catch (error) {
         console.log(error.message);
@@ -26,6 +29,24 @@ export default function DashPosts() {
       fetchPosts();
     }
   }, [currentUser._id]);
+
+  const handleShowMore = async () => {
+    const startIndex = userPosts.length;
+    try {
+      const res = await fetch(`/api/post/getposts?userId=${currentUser._id}&startIndex=${startIndex}`);
+      const data = await res.json();
+      
+      if(res.ok){
+        setUserPosts((prev)=>[...prev, ...data.posts]);
+        if(data.posts.length < 9){
+          setShowMore(false);
+        }
+      }
+
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
 
   return (
     <div className="table-auto overflow-x-scroll md:mx-auto p-3 scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500">
@@ -76,6 +97,13 @@ export default function DashPosts() {
               </Table.Body>;
             })}
           </Table>
+          {
+            showMore && (
+              <button onClick={handleShowMore} className="w-full text-teal-500 self-center text-sm py-7">
+                Show more
+              </button>
+            )
+          }
         </>
       ) : (
         <p>you have no posts yet!</p>
