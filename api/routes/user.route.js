225a7fc4462d22b86fec60341a526c2a6fdc643f
Menase7 <menasedebel7@gmail.com>
2024-04-09 16:51:13 +0300

    show the comments of a post

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import { verifyToken } from '../utils/verifyUser.js'; // Corrected import path
-import { deleteUser, updateUser, signout, getusers } from '../controllers/user.controller.js'; // Assuming controller file is .js
+import { deleteUser, updateUser, signout, getusers, getUser } from '../controllers/user.controller.js'; // Assuming controller file is .js
 
 const router = express.Router();
 
 router.get('/getusers', verifyToken, getusers);
+router.get(':userId', getUser)
 router.put('/update/:userId', verifyToken, updateUser);
 router.delete('/delete/:userId', verifyToken, deleteUser);
 router.post('/signout', signout);
