58f2b80fcb43ebf126fadeef499940d24f0d60fe
Menase7 <menasedebel7@gmail.com>
2024-04-13 22:32:25 +0300

    modification on index file

--- api/index.js
+++ api/index.js
@@ -37,7 +37,7 @@ app.use('/api/user', userRoute);
 app.use('/api/post', postRoute);
 app.use('/api/comment', commentRoute);
 
-app.use(express.static(path.join(__dirname, '/client/dist')));
+app.use(express.static(path.join(__dirname, 'client', 'dist')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
